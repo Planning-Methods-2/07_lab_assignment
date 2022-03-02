b95db3e57a00d3887208e0a8194c5a07feac3ec6
Esteban Lopez Ochoa <estebanlp@users.noreply.github.com>
2022-03-02 17:43:32 -0600

    Initial commit

---
+++ Lab6_Assignment.R
@@ -0,0 +1,40 @@
+# Lab 6 Assignment: Chi-Square and Correlation
+# The University of Texas at San Antonio
+# URP-5393: Urban Planning Methods II
+
+
+#---- Instructions ----
+
+# 1. [70 points] Open the R file "Lab6_Assignment.R" and answer the questions bellow
+# 2. [30 points] Run a Chi-Square and Correlation analysis in your data used to the final project. 
+
+
+#---- Part 1.
+
+# 1.1 load the same household data used in the Lab6_Script.R file, create the object `HTS`
+
+
+
+# 2. Create an object called `HTS_tab` that has the frequency or contingency table for the variables `htype` and `income_cat`
+# Refer to the BQRM book (p.13) if you don't know what the variables mean
+
+
+
+
+#3. run a Chi-Square test to show if the two variables above are associated or not. Provide an interpretation of the p-value and a sentence stating if the null hypothesis of no association should be rejected or not. What conclusions can you draw?
+
+
+
+#4. Calculate the Cramer's V coefficient for your Chi-Square test. What conclusions can you draw?
+
+
+
+#5. Calculate the pearson correlation coefficient between the variables `veh` and `hhincome`. What can you say about the relationship between these two variables?
+
+
+
+#6. Calculate the spearman correlation coefficient between the variables `hhsize` and `income_cat`. What can you say about the relationship between these two variables?
+
+
+#---- Part 2.
+# The more you do to explore your data sets with these two new tools, the more you will learn and become familiar. If you do only the minimum (calculate a chi-Square test and a correlation coefficient) is fine, but the more you do to explore the relationships that should be in your conceptual framework, the better.
---
+++ README.md
@@ -0,0 +1,22 @@
+# Lab 6: Chi-Square test and Correlation Analysis with R
+
+## Objectives:
+
+In this assignment you will practice to:
+
+1. Learn to apply Chi-Square tests on categorical data
+2. Learn to calculate Correlation coefficients
+
+
+Any questions, do not hesitate to e-mail me at esteban.lopezochoa at utsa dot edu
+
+
+## Instructions:
+
+To submit your Lab 6 assignment. 
+
++ Open the file "Lab6_assignment.R"
++ Follow the steps explained in the file `Assignment_Instructions.pdf` to:
+  - accept the assignment using the link that you will receive
+  - clone your assignment repository and create an RStudio project in your computer
+  - commit your progress as you move forward
